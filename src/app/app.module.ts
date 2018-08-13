b478672802926c44f8b837390f856f37c9f2fe61
Sanyyouisf <sanymamdoh@yahoo.com>
2018-08-13 16:47:49 -0500

    backgroung

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { environment } from './../environments/environment';
+// import { AngularFireModule} from 'angularfire2';
+// import { AngularFireDatabaseModule } from 'angularfire2/database'; 
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -15,7 +18,9 @@ import { BodyComponent } from './body/body.component';
     BodyComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
